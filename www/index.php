2da9ec5509d90c4dc5c080e5ea46e41d7ca62ea8
Bob Wallis <bob.wallis@gmail.com>
2010-10-20 16:55:36 +0100

    Move around class loader initialisation

--- www/index.php
+++ www/index.php
@@ -10,8 +10,6 @@ define( 'ACTION_PATH', __DIR__.'/../application/Actions' );
 // Define class autoloaders
 require( LIBRARY_PATH.'/Blueline/ClassLoader.php' );
 $classLoader_Blueline = new ClassLoader( 'Blueline', LIBRARY_PATH );
-$classLoader_Models = new ClassLoader( 'Models', APPLICATION_PATH );
-$classLoader_Helpers = new ClassLoader( 'Helpers', LIBRARY_PATH );
 
 // Load application configuration
 require( APPLICATION_PATH.'/config.php' );
@@ -23,15 +21,21 @@ Cache::initialise();
 if( Cache::exists( 'static', Response::id() ) ) {
 	Response::body( Cache::get( 'static', Response::id() ) );
 	Response::send();
+	exit();
 }
+
+// Some more class autoloaders
+$classLoader_Models = new ClassLoader( 'Models', APPLICATION_PATH );
+$classLoader_Helpers = new ClassLoader( 'Helpers', LIBRARY_PATH );
+
 // Check the dynamic cache for a response
-elseif( Cache::exists( 'dynamic', Response::id() ) ) {
+if( Cache::exists( 'dynamic', Response::id() ) ) {
 	eval( cache::read( 'dynamic', Response::id() ) );
 	exit();
 }
-else {
-	Database::initialise();
-	Action::execute();
-	View::create();
-	Response::send();
-}
+
+
+Database::initialise();
+Action::execute();
+View::create();
+Response::send();
