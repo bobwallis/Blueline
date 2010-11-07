fa36c20944c3abac82b589bab160bb79c001c172
Bob Wallis <bob.wallis@gmail.com>
2010-11-07 21:31:20 +0000

    Make HTML suggestions be a snippet

--- application/Templates/views/services/suggest.html.php
+++ application/Templates/views/services/suggest.html.php
@@ -1,7 +1,11 @@
-<li>
+<?php
+namespace Blueline;
+View::layout( 'blank' );
+?>
+<ul>
 <?php
 for( $i = 0; $i < count( $suggestions['queries'] ); ++$i ) {
 	echo "<li><a href=\"{$suggestions['URLs'][$i]}\">{$suggestions['queries'][$i]}</a></li>\n";
 }
 ?>
-</li>
+</ul>
