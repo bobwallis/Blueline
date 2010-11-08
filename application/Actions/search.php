9bb5b301eb95f52f45733d99fb21048a95b6b71a
Bob Wallis <bob.wallis@gmail.com>
2010-11-08 16:24:06 +0000

    Remove unnecessary variables

--- application/Actions/search.php
+++ application/Actions/search.php
@@ -4,19 +4,16 @@ use \Models\Association, \Models\Method, \Models\Tower;
 
 Model::$_searchLimit = 10;
 $associationSearchResults = Association::search();
-$associationSearchCount = ( count( $associationSearchResults ) > 0 )? Association::searchCount() : 0;
 $methodSearchResults = Method::search();
-$methodSearchCount = ( count( $methodSearchResults ) > 0 )? Method::searchCount() : 0;
 $towerSearchResults = Tower::search();
-$towerSearchCount = ( count( $towerSearchResults ) > 0 )? Tower::searchCount() : 0;
 
 Response::cacheType( 'static' );
 View::set( 'associations', $associationSearchResults );
-View::set( 'associationCount', $associationSearchCount );
+View::set( 'associationCount', ( count( $associationSearchResults ) > 0 )? Association::searchCount() : 0 );
 View::set( 'methods', $methodSearchResults );
-View::set( 'methodCount', $methodSearchCount );
+View::set( 'methodCount', ( count( $methodSearchResults ) > 0 )? Method::searchCount() : 0 );
 View::set( 'towers', $towerSearchResults );
-View::set( 'towerCount', $towerSearchCount );
+View::set( 'towerCount', ( count( $towerSearchResults ) > 0 )? Tower::searchCount() : 0 );
 
 View::set( 'q', isset( $_GET['q'] )? $_GET['q'] : '' );
 View::set( 'queryString', Request::queryString() );
