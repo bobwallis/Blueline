9d7a01325cafb0663c3f40eb43d009fb2cc15750
Bob Wallis <bob.wallis@gmail.com>
2010-11-09 10:42:18 +0000

    Allow extra arguments to specify which section is required

--- application/Actions/methods/view.php
+++ application/Actions/methods/view.php
@@ -4,8 +4,13 @@ use \Models\Method;
 
 // Redirect to /methods on empty request
 if( !isset( $arguments[0] ) || empty( $arguments[0] ) ) {
+	Response::cacheType( 'dynamic' );
 	Response::redirect( '/methods' );
 	return;
+}
+// 404 if there are more than two arguments, or if the second argument is invalid
+if( isset( $arguments[2] ) || !in_array( $arguments[1], array( 'grid' ) ) ) {
+	throw new Exception( 'Bad arguments', 404 );
 }
 
 // Try and find methods matching the argument(s)
@@ -18,16 +23,18 @@ $methodDetails = array_map(
 // If only one method has been requested, and it hasn't been found, then 404
 if( count( $methodDetails ) == 1 && $methodDetails[0]['title'] == 'Not Found' ) {
 	throw new Exception( 'Method not found', 404 );
-	return;
 }
 
 // If the URL could be neater, then redirect to the neater version
 $tidyArgument = implode( '|', array_map( function( $m ) { return str_replace( ' ', '_', $m['title'] ); }, $methodDetails ) );
 if( strcmp( $arguments[0], $tidyArgument ) != 0 ) {
 	Response::cacheType( 'dynamic' );
-	Response::redirect( '/methods/view/'.$tidyArgument );
+	Response::redirect( '/methods/view/'.$tidyArgument . ( isset( $arguments[1] )? '/'.$arguments[1] : '' ) );
 }
 
 // Export data to the view for successful request
 Response::cacheType( 'static' );
 View::set( 'methods', $methodDetails );
+if( isset( $arguments[1] ) ) {
+	View::view( '/methods/view.'.$arguments[1] );
+}
