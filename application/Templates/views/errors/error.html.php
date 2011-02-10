4b890024b64894e4cb8d534ba0f89d0073969f16
Bob Wallis <bob.wallis@gmail.com>
2011-02-10 19:25:28 +0000

    Fix layout of error pages

--- application/Templates/views/errors/error.html.php
+++ application/Templates/views/errors/error.html.php
@@ -12,6 +12,8 @@ View::element( 'default.header', array(
 <header>
 	<h1><?php echo $errorTitle; ?></h1>
 </header>
-<p>Try a search, or visit the homepage to find what you're looking for.</p>
-<?php echo ( Config::get( 'development' ) && isset( $errorMessage ) )? '<p>'.str_replace( "\n", "<br />\n", $errorMessage ).'</p>' : ''; ?>
+<div class="content wallOfText">
+	<p>Try a search, or visit the homepage to find what you're looking for.</p>
+	<?php echo ( Config::get( 'development' ) && isset( $errorMessage ) )? '<p>'.str_replace( "\n", "<br />\n", $errorMessage ).'</p>' : ''; ?>
+</div>
 <?php View::element( 'default.footer' ); ?>
