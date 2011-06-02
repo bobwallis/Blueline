87990200d4ac6622dd88385233d2da5e2b077b03
Bob Wallis <bob.wallis@gmail.com>
2011-06-02 21:02:40 +0100

    Index page gets cached anyway, don't need to include

--- application/Templates/views/site.manifest.php
+++ application/Templates/views/site.manifest.php
@@ -7,7 +7,6 @@ View::cache( 43200 );
 CACHE MANIFEST
 # <?=$this->get( 'timestamp' )."\n"?>
 CACHE:
-/
 <?php foreach( $this->get( 'resources' ) as $resource ) : ?>
 <?=$resource."\n"?>
 <?php endforeach; ?>
