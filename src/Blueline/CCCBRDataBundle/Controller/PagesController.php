2c997346fd6058e75937580afa078a5a3877815a
Bob Wallis <bob.wallis@gmail.com>
2011-08-04 20:55:50 +0100

    About and copyright pages

--- src/Blueline/CCCBRDataBundle/Controller/PagesController.php
+++ src/Blueline/CCCBRDataBundle/Controller/PagesController.php
@@ -6,10 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
 class PagesController extends Controller {
-    
+
 	public function welcomeAction() {
 		$format = $this->getRequest()->getRequestFormat();
 		
 		return $this->render( 'BluelineCCCBRDataBundle:Pages:welcome.'.$format.'.twig' );
+	}
+	
+	public function pageAction( $page ) {
+		$format = $this->getRequest()->getRequestFormat();
+		
+		return $this->render( 'BluelineCCCBRDataBundle:Pages:'.$page.'.'.$format.'.twig' );
 	}
 }
