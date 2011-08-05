af0475ff9e4087e88f3eaebf6ba1d9586aa52242
Bob Wallis <bob.wallis@gmail.com>
2011-08-05 15:48:30 +0100

    Use APC autoloader

--- app/autoload.php
+++ app/autoload.php
@@ -1,9 +1,10 @@
 <?php
+require_once __DIR__.'/../vendor/symfony/src/Symfony/Component/ClassLoader/ApcUniversalClassLoader.php';
 
-use Symfony\Component\ClassLoader\UniversalClassLoader;
+use Symfony\Component\ClassLoader\ApcUniversalClassLoader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-$loader = new UniversalClassLoader();
+$loader = new ApcUniversalClassLoader('blueline_autoloader.');
 $loader->registerNamespaces(array(
     'Symfony'          => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
     'Sensio'           => __DIR__.'/../vendor/bundles',
