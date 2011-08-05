b8222eeedb7078a1b2408b3efb5402dc794810ba
Bob Wallis <bob.wallis@gmail.com>
2011-08-05 15:52:42 +0100

    Use ?snippet=1 URLs for content

--- src/Blueline/CCCBRDataBundle/Controller/PagesController.php
+++ src/Blueline/CCCBRDataBundle/Controller/PagesController.php
@@ -10,13 +10,14 @@ class PagesController extends Controller {
 	public function welcomeAction() {
 		$format = $this->getRequest()->getRequestFormat();
 		
-		return $this->render( 'BluelineCCCBRDataBundle:Pages:welcome.'.$format.'.twig' );
+		return $this->render( 'BluelineCCCBRDataBundle:Pages:welcome.layout.'.$format.'.twig' );
 	}
 	
 	public function pageAction( $page ) {
 		$request = $this->getRequest();
 		$format = $request->getRequestFormat();
+		$isSnippet = $format == 'html' && $request->query->get( 'snippet' );
 		
-		return $this->render( 'BluelineCCCBRDataBundle:Pages:'.$page.'.'.$format.'.twig' );
+		return $this->render( 'BluelineCCCBRDataBundle:Pages:'.$page.($isSnippet?'':'.layout').'.'.$format.'.twig' );
 	}
 }
