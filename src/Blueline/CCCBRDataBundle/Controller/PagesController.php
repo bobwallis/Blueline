5faa85b902b9a6f15577c3b6ecfd0258c0636ab8
Bob Wallis <bob.wallis@gmail.com>
2011-08-16 16:22:44 +0100

    Add routes for searches, robots.txt, humans.txt

--- src/Blueline/CCCBRDataBundle/Controller/PagesController.php
+++ src/Blueline/CCCBRDataBundle/Controller/PagesController.php
@@ -8,16 +8,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class PagesController extends Controller {
 
 	public function welcomeAction() {
-		$format = $this->getRequest()->getRequestFormat();
-		
-		return $this->render( 'BluelineCCCBRDataBundle:Pages:welcome.layout.'.$format.'.twig' );
+		return $this->render( 'BluelineCCCBRDataBundle:Pages:welcome.layout.html.twig' );
 	}
 	
 	public function pageAction( $page ) {
 		$request = $this->getRequest();
 		$format = $request->getRequestFormat();
-		$isSnippet = $format == 'html' && $request->query->get( 'snippet' );
+		$isLayout = $format == 'html' && !$request->query->get( 'snippet' );
 		
-		return $this->render( 'BluelineCCCBRDataBundle:Pages:'.$page.($isSnippet?'':'.layout').'.'.$format.'.twig' );
+		return $this->render( 'BluelineCCCBRDataBundle:Pages:'.$page.($isLayout?'.layout':'').'.'.$format.'.twig' );
 	}
 }
---
+++ src/Blueline/CCCBRDataBundle/Controller/SearchController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Blueline\CCCBRDataBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class SearchController extends Controller {
+
+	public function globalAction() {
+	}
+	
+	public function associationsAction() {
+	}
+	
+	public function methodsAction() {
+	}
+	
+	public function towersAction() {
+	}
+}
