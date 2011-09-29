025de24b2970ae945472f1d1196ead6a31b81bf6
Bob Wallis <bob.wallis@gmail.com>
2011-09-29 18:59:27 +0100

    Refactor and simplify hotkeys

--- web/scripts/ui/Hotkeys.js
+++ web/scripts/ui/Hotkeys.js
@@ -1,118 +1,99 @@
 /*global require: false, define: false, google: false */
-define( ['../helpers/jquery.hotkeys'], function() {
-	var $document = $( document ),
-		$window = $( window );
-	var Hotkeys = {
-		addGlobal: function( keys, fn ) {
-			 $document.bind( 'keydown', keys, fn );
-		},
-		add: function( el, keys, fn ) {
-			$( el ).bind( 'keydown', keys, fn );
-		}
-	};
+define( function() {
+	/** @const */ var HOME = 36;
+	/** @const */ var END = 35;
+	/** @const */ var PGUP = 33;
+	/** @const */ var PGDOWN = 34;
+	/** @const */ var SLASH = 191;
+	/** @const */ var UP = 38;
+	/** @const */ var DOWN = 40;
+	/** @const */ var RETURN = 13;
 	
-	var bigQ = $( '#bigQ' );
-	// Add search related hotkeys
-	Hotkeys.addGlobal( '/', function( e ) { if( bigQ.is( ':visible' ) ) { e.preventDefault(); e.stopPropagation(); bigQ.focus(); } } );
-	
-	var clearSelection = function() { $( 'ol.searchResults li.selected' ).removeClass( 'selected' ); },
-		selectFirst = function( e ) {
-			e.preventDefault();
-			$( 'ol.searchResults:first li:first' ).addClass( 'selected' );
-			bigQ.blur();
-		},
-		selectNext = function( e ) {
-			var current = $( 'ol.searchResults li.selected:first' ),
-				next = current.next( 'li' );
-			if( current.length > 0 ) {
-				if( next.length > 0 ) {
-					e.preventDefault();
-					current.removeClass( 'selected' );
-					next.addClass( 'selected' );
-				}
-				else {
-					var allItems = $( 'ol.searchResults li' );
-					if( !allItems.last().is( '.selected' ) ) {
-						for( var i = 0; i < allItems.length; ++i ) {
-							if( $( allItems[i] ).hasClass( 'selected' ) ) {
-								e.preventDefault();
-								current.removeClass( 'selected' );
-								next = $( allItems[i+1] )
-								next.addClass( 'selected' );
-								break;
-							}
+	$( function() {
+		var $window = $( window ),
+			$bigQ = $( '#bigQ' ),
+			$smallQ = $( '#smallQ' );
+		
+		$( window ).keydown( function( e ) {
+			switch( e.which ) {
+				// Hotkeys to control paging links
+				case HOME:
+					$( "div.pagingLinks:first a:contains('1'):first" ).click();
+					break;
+				case PGUP:
+					$( "div.pagingLinks:first a:contains('«'):first" ).click();
+					break;
+				case PGDOWN:
+					$( "div.pagingLinks:first a:contains('»'):first" ).click();
+					break;
+				case END:
+					$( "div.pagingLinks:first :not(:contains('»')):last" ).click();
+					break;
+				
+				// Hotkeys to control search navigation using the keyboard
+				case UP:
+					var current = $( 'li.selected:first' ),
+						prev = current.prev( 'li' );
+					if( current.length > 0 ) {
+						e.preventDefault();
+						if( prev.length > 0 ) {
+							current.removeClass( 'selected' );
+							prev.addClass( 'selected' );
+						// Scroll into view if not visible
+							var prevOffsetTop = prev.offset().top,
+								prevOffsetBottom = prevOffsetTop + prev.outerHeight(),
+								windowScrollTop = $window.scrollTop(),
+								windowScrollBottom = windowScrollTop + $window.height();
+							if( prevOffsetTop < windowScrollTop ) { $( 'html, body' ).animate( { scrollTop: prevOffsetTop-2 }, 75 ); }
+							else if( prevOffsetBottom > windowScrollBottom ) { $( 'html, body' ).animate( { scrollTop: windowScrollTop+2+(prevOffsetBottom-windowScrollBottom) }, 75 ); }
+						}
+						else {
+							bigQ.focus();
 						}
 					}
-				}
-				// Scroll into view if not visible
-				if( next.length > 0 ) {
-					var nextOffsetTop = next.offset().top,
-						nextOffsetBottom = nextOffsetTop + next.outerHeight(),
-						windowScrollTop = $window.scrollTop(),
-						windowScrollBottom = windowScrollTop + $window.height();
-					if( nextOffsetTop < windowScrollTop ) { $( 'html, body' ).animate( { scrollTop: nextOffsetTop-2 }, 75 ); }
-					else if( nextOffsetBottom > windowScrollBottom ) { $( 'html, body' ).animate( { scrollTop: windowScrollTop+2+(nextOffsetBottom-windowScrollBottom) }, 75 ); }
-				}
-			}
-		},
-		selectPrevious = function( e ) {
-			var current = $( 'ol.searchResults li.selected:first' ),
-				prev = current.prev( 'li' );
-			if( current.length > 0 ) {
-				e.preventDefault();
-				if( prev.length > 0 ) {
-					current.removeClass( 'selected' );
-					prev.addClass( 'selected' );
-				}
-				else {
-					var allItems = $( 'ol.searchResults li' );
-					if( !allItems.first().is( '.selected' ) ) {
-						for( var i = 0; i < allItems.length; ++i ) {
-							if( $( allItems[i] ).hasClass( 'selected' ) ) {
-								current.removeClass( 'selected' );
-								prev = $( allItems[i-1] );
-								prev.addClass( 'selected' );
-								break;
-							}
-						}
+					break;
+				case DOWN:
+					var current = $( 'li.selected:first' ), next;
+					if( $bigQ.is( ':visible:focus' ) || current.length == 0 ) {
+						next = $( 'section.search:first ol:first li:first' );
+						$bigQ.blur();
 					}
 					else {
-						bigQ.focus();
+						next = current.next( 'li ' );
 					}
-				}
-				// Scroll into view if not visible
-				if( prev.length > 0 ) {
-					var prevOffsetTop = prev.offset().top,
-						prevOffsetBottom = prevOffsetTop + prev.outerHeight(),
-						windowScrollTop = $window.scrollTop(),
-						windowScrollBottom = windowScrollTop + $window.height();
-					if( prevOffsetTop < windowScrollTop ) { $( 'html, body' ).animate( { scrollTop: prevOffsetTop-2 }, 75 ); }
-					else if( prevOffsetBottom > windowScrollBottom ) { $( 'html, body' ).animate( { scrollTop: windowScrollTop+2+(prevOffsetBottom-windowScrollBottom) }, 75 ); }
-				}
+					if( next.length > 0 ) {
+						e.preventDefault();
+						current.removeClass( 'selected' );
+						next.addClass( 'selected' );
+						// Scroll into view if not visible
+						var nextOffsetTop = next.offset().top,
+							nextOffsetBottom = nextOffsetTop + next.outerHeight(),
+							windowScrollTop = $window.scrollTop(),
+							windowScrollBottom = windowScrollTop + $window.height();
+						if( nextOffsetTop < windowScrollTop ) { $( 'html, body' ).animate( { scrollTop: nextOffsetTop-2 }, 75 ); }
+						else if( nextOffsetBottom > windowScrollBottom ) { $( 'html, body' ).animate( { scrollTop: windowScrollTop+2+(nextOffsetBottom-windowScrollBottom) }, 75 ); }
+					}
+					break;
+				case RETURN:
+					$( 'li.selected:first a:first' ).click();
+					break;
+				
+				// Focus search boxes on '/'
+				case SLASH:
+					// Focus the search box if it isn't already focussed
+					if( $bigQ.is( ':visible:not(:focus)' ) ) {
+						e.preventDefault();
+						$bigQ.focus();
+					}
+					else if( $smallQ.is( ':visible:not(:focus)' ) ) {
+						e.preventDefault();
+						$smallQ.focus();
+					}
+					break;
 			}
-		}
-		clickSelected = function() {
-			$( 'ol.searchResults li.selected:first a:first' ).click();
-		};
-	bigQ.focus( clearSelection );
-	Hotkeys.add( bigQ, 'down', selectFirst );
-	Hotkeys.addGlobal( 'down', selectNext );
-	Hotkeys.addGlobal( 'up', selectPrevious );
-	Hotkeys.addGlobal( 'return', clickSelected );
-	
-	// Add hotkeys to click paging links
-	var clickFirst = function() { $( "div.pagingLinks:first a:contains('1'):first" ).click(); },
-		clickLast = function() { $( "div.pagingLinks:first :not(:contains('»')):last" ).click(); },
-		clickPrevious = function() { $( "div.pagingLinks:first a:contains('«'):first" ).click(); },
-		clickNext = function() {console.log('next'); $( "div.pagingLinks:first a:contains('»'):first" ).click(); };
-	Hotkeys.addGlobal( 'home', clickFirst );
-	Hotkeys.add( bigQ, 'home', clickFirst );
-	Hotkeys.addGlobal( 'pageup', clickPrevious );
-	Hotkeys.add( bigQ, 'pageup', clickPrevious );
-	Hotkeys.addGlobal( 'pagedown', clickNext );
-	Hotkeys.add( bigQ, 'pagedown', clickNext );
-	Hotkeys.addGlobal( 'end', clickLast );
-	Hotkeys.add( bigQ, 'end', clickLast );
-	
-	return Hotkeys;
+		} );
+		
+		// Clear search keyboard navigation when selecting search box
+		$bigQ.focus( function() { $( 'li.selected' ).removeClass( 'selected' ); } );
+	} );
 } );
