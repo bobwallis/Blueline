98444f7f5332b3fcd834184476230adb7943062b
Bob Wallis <bob.wallis@gmail.com>
2011-10-16 18:27:56 +0100

    Set click events on tabs not tab bars.

--- web/scripts/ui/TabBar.js
+++ web/scripts/ui/TabBar.js
@@ -2,7 +2,7 @@
 define( function() {
 	var $window = $( window );
 
-	var tabBarClick = function( e ) {
+	var tabClick = function( e ) {
 		var target = $( e.target );
 		if( !target.is( 'li' ) ) { return; }
 		
@@ -18,14 +18,14 @@ define( function() {
 
 	var TabBar = function( options ) {
 		var $container = $( '#'+options.landmark+'_' );
-		if( $container.size() == 0 ) {
+		if( $container.length == 0 ) {
 			$container = $( '<ul id="'+options.landmark+'_" class="tabBar">'+ options.tabs.map( function( t, i ) {
 				return '<li id="tab_'+t.content+'"'+(t.className? ' class="'+t.className+'"' : '')+'>'+t.title+'</li>';
 			} ).join( '' ) + '</ul>' );
 			$( $container.children()[(typeof options.active === 'number' )?options.active:0] ).addClass( 'active' );
 			$( '#'+options.landmark ).replaceWith( $container );
 		}
-		$container.click( tabBarClick );
+		$container.children().click( tabClick ); // Add click event to each child rather than just the ul so highlight-on-tap works properly on Android/iOS
 	};
 	
 	// Expose the TabBar function
