399281cfba1f024b6d3c4dacee6d766ee336b343
Bob Wallis <bob.wallis@gmail.com>
2011-10-22 19:50:04 +0100

    Actually fix page title setting

--- web/scripts/ui/Window.js
+++ web/scripts/ui/Window.js
@@ -4,25 +4,17 @@ define( function() {
 	
 	Window = {
 		update: function( url ) {
-			var pageTitle = $( '#content h1' ).map( function( i, e ) { return $(e).text(); } ),
-				section = sectionRegExp.exec( url );
+			var pageTitle = $.makeArray( $( '#content h1' ).map( function( i, e ) { return $(e).text(); } ) ).join( ', ' ),
+				section = sectionRegExp.exec( url ),
+				title = '';
 			
-			switch( pageTitle.length ) {
-				case 0:
-					pageTitle = '';
-					break;
-				case 1:
-					pageTitle = pageTitle[0];
-					break;
-				default:
-					pageTitle = pageTitle.join( ', ' );
-					break;
+			if( pageTitle !== '' ) {
+				title = pageTitle + ' | ';
 			}
-			section = (section == null)? '' : section[1].charAt(0).toUpperCase() + section[1].slice( 1 );
-			
-			if( section == pageTitle ) { pageTitle = ''; }
-			
-			Window.title( ( pageTitle	+ ' | ' + section + ' | Blueline' ).replace( /^[\s\|]*/, '' ).replace( /[\s\|]*$/, '' ).replace( '| |', '|' ) );
+			if( section !== null ) {
+				title += section[1].charAt(0).toUpperCase() + section[1].slice( 1 ) + ' | ';
+			}
+			Window.title( title + 'Blueline' );
 		},
 		title: function( set ) {
 			if( typeof set === 'string' ) {
