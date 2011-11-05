8b28bd4808f9cce8a93bff84bf2e9c9a1e5281fe
Bob Wallis <bob.wallis@gmail.com>
2011-11-05 11:38:55 +0000

    Fix title on search pages

--- web/scripts/ui/Window.js
+++ web/scripts/ui/Window.js
@@ -1,15 +1,20 @@
 /*global require: false, define: false, google: false, History: false */
 define( function() {
-	var sectionRegExp = /\/(associations|methods|towers)(\/|$)/;
+	var sectionRegExp = /\/(associations|methods|towers)(\/|$)/,
+		searchRegExp = /\/(associations|methods|towers)\/search/;
 	
 	Window = {
 		update: function( url ) {
 			var pageTitle = $.makeArray( $( '#content h1' ).map( function( i, e ) { return $(e).text(); } ) ).join( ', ' ),
 				section = sectionRegExp.exec( url ),
+				search = searchRegExp.exec( url ),
 				title = '';
 			
 			if( pageTitle !== '' ) {
-				title = pageTitle + ' | ';
+				title += pageTitle + ' | ';
+			}
+			if( search !== null ) {
+				title += 'Search | ';
 			}
 			if( section !== null ) {
 				title += section[1].charAt(0).toUpperCase() + section[1].slice( 1 ) + ' | ';
