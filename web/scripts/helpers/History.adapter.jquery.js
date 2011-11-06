aece377348fc9dbfd84c3a9abbf693bb49d16c10
Bob Wallis <bob.wallis@gmail.com>
2011-11-06 11:02:42 +0000

    RequireJS 1.0.1, jQuery 1.7, History.js 1.7.1

--- web/scripts/helpers/History.adapter.jquery.js
+++ web/scripts/helpers/History.adapter.jquery.js
@@ -7,6 +7,7 @@
 
 
 define( function() {
+	"use strict";
 	// Localise Globals
 	var
 		History = window.History = window.History||{},
@@ -21,10 +22,10 @@ define( function() {
 	History.Adapter = {
 		/**
 		 * History.Adapter.bind(el,event,callback)
-		 * @param {Element|Selector} el
-		 * @param {String} event - custom and standard events
-		 * @param {Function} callback
-		 * @return
+		 * @param {Element|string} el
+		 * @param {string} event - custom and standard events
+		 * @param {function} callback
+		 * @return {void}
 		 */
 		bind: function(el,event,callback){
 			jQuery(el).bind(event,callback);
@@ -32,18 +33,34 @@ define( function() {
 
 		/**
 		 * History.Adapter.trigger(el,event)
-		 * @param {Element|Selector} el
-		 * @param {String} event - custom and standard events
-		 * @return
+		 * @param {Element|string} el
+		 * @param {string} event - custom and standard events
+		 * @param {Object=} extra - a object of extra event data (optional)
+		 * @return {void}
 		 */
-		trigger: function(el,event){
-			jQuery(el).trigger(event);
+		trigger: function(el,event,extra){
+			jQuery(el).trigger(event,extra);
 		},
 
 		/**
-		 * History.Adapter.trigger(el,event,data)
-		 * @param {Function} callback
-		 * @return
+		 * History.Adapter.extractEventData(key,event,extra)
+		 * @param {string} key - key for the event data to extract
+		 * @param {string} event - custom and standard events
+		 * @param {Object=} extra - a object of extra event data (optional)
+		 * @return {mixed}
+		 */
+		extractEventData: function(key,event,extra){
+			// jQuery Native then jQuery Custom
+			var result = (event && event.originalEvent && event.originalEvent[key]) || (extra && extra[key]) || undefined;
+
+			// Return
+			return result;
+		},
+
+		/**
+		 * History.Adapter.onDomLoad(callback)
+		 * @param {function} callback
+		 * @return {void}
 		 */
 		onDomLoad: function(callback) {
 			jQuery(callback);
@@ -54,5 +71,4 @@ define( function() {
 	if ( typeof History.init !== 'undefined' ) {
 		History.init();
 	}
-
 });
