deca5a75d864fdd81b81aa8c3deb0ef6b1a169e2
Bob Wallis <bob.wallis@gmail.com>
2011-11-20 19:47:59 +0000

    Handle orientation changes internally, load local links

--- resources/android/app/src/uk/me/rsw/blueline/BluelineActivity.java
+++ resources/android/app/src/uk/me/rsw/blueline/BluelineActivity.java
@@ -1,10 +1,13 @@
 package uk.me.rsw.blueline;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 public class BluelineActivity extends Activity {
@@ -13,31 +16,57 @@ public class BluelineActivity extends Activity {
 	
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
         BluelineWebView = (WebView) findViewById(R.id.webview);
         BluelineHeader = (TextView) findViewById(R.id.header);
 
-        WebSettings webSettings = BluelineWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setAppCacheMaxSize(524288);
-        webSettings.setAppCachePath("/data/data/uk.me.rsw.blueline/cache");
-        webSettings.setAllowFileAccess(true);
-        webSettings.setAppCacheEnabled(true);
-        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
-        webSettings.setDatabasePath("/data/data/uk.me.rsw.blueline/cache");
-        webSettings.setDatabaseEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-        webSettings.setGeolocationEnabled(true);
-        webSettings.setSupportZoom(true);
-        webSettings.setSaveFormData(false);
-        webSettings.setUserAgentString("Blueline "+webSettings.getUserAgentString());
-        
-        BluelineWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-               
-        BluelineWebView.loadUrl("http://blueline.rsw.me.uk/?android");
+        if (savedInstanceState != null) {
+        	BluelineWebView.restoreState(savedInstanceState);
+        }
+        else {
+	        WebSettings webSettings = BluelineWebView.getSettings();
+	        webSettings.setJavaScriptEnabled(true);
+	        webSettings.setAppCacheMaxSize(524288);
+	        webSettings.setAppCachePath("/data/data/uk.me.rsw.blueline/cache");
+	        webSettings.setAllowFileAccess(true);
+	        webSettings.setAppCacheEnabled(true);
+	        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+	        webSettings.setDatabasePath("/data/data/uk.me.rsw.blueline/cache");
+	        webSettings.setDatabaseEnabled(true);
+	        webSettings.setDomStorageEnabled(true);
+	        webSettings.setGeolocationEnabled(true);
+	        webSettings.setSupportZoom(true);
+	        webSettings.setSaveFormData(false);
+	        webSettings.setUserAgentString("Blueline "+webSettings.getUserAgentString());
+	        
+	        BluelineWebView.setWebViewClient(new BluelineWebViewClient());
+	        BluelineWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+	               
+	        BluelineWebView.loadUrl("http://blueline.rsw.me.uk/?android");
+        }
+    }
+    
+    /** Web Client */
+    private class BluelineWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (Uri.parse(url).getHost().equals("blueline.rsw.me.uk")) {
+                // Don't override local links
+                return false;
+            }
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
+    }
+    
+    /** Handle orientation changes */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+    	BluelineWebView.saveState(outState);
     }
     
     /** Handle back button presses */
