f8ad5938dbc0ff03b0b2b66b8cf28e0a8e59dcfe
Bob Wallis <bob.wallis@gmail.com>
2011-12-04 11:18:11 +0000

    Detect Chrome web app

--- web/scripts/helpers/Is.js
+++ web/scripts/helpers/Is.js
@@ -16,7 +16,7 @@ define( function() {
 			return false;
 		},
 		app: function() {
-			return ( (('standalone' in navigator) && navigator.standalone) || typeof window['Android'] === 'object' );
+			return ( (('chrome' in window) && window.chrome.isInstalled) || (('standalone' in navigator) && navigator.standalone) || typeof window['Android'] === 'object' );
 		}
 	};
 } );
