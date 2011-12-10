34bea42211298c04b4bbb874b514daa32c07e9f8
Bob Wallis <bob.wallis@gmail.com>
2011-12-10 15:33:56 +0000

    Clear db cache on each load to make developing easier

--- web/scripts/helpers/Page/Cache.js
+++ web/scripts/helpers/Page/Cache.js
@@ -1,6 +1,8 @@
 define( ['../Can', './Cache/IndexedDB', './Cache/WebSQL', './Cache/Null'], function( Can, ContentCache_indexedDB, ContentCache_webSQL, ContentCache_null ) {
-	// The resources/scripts/buildWWW script will update this line automatically
-	var contentCache_born = (new Date( 2011, 11-1, 20 ).toDateString());
+	// The resources/scripts/buildWWW script will update this line automatically 
+	// to be the date that the scripts were built. For development, use a random 
+	// number so the cache is cleared each load.
+	var contentCache_born = (Math.floor(Math.random()*101)).toString();
 
 	// Return ContentCache depending on support
 	var ContentCache;
