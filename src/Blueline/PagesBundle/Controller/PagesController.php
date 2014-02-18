abb2ec2d1be7c0a443708f0b71e73a818392aaf6
Bob Wallis <bob.wallis@gmail.com>
2014-02-18 20:53:38 +0000

    Experiments with ETags

--- src/Blueline/PagesBundle/Controller/PagesController.php
+++ src/Blueline/PagesBundle/Controller/PagesController.php
@@ -2,6 +2,7 @@
 namespace Blueline\PagesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class PagesController extends Controller
 {
@@ -9,14 +10,15 @@ class PagesController extends Controller
     {
         $request = $this->getRequest();
         $format = $request->getRequestFormat();
-        $response = $this->render( 'BluelinePagesBundle::'.$page.'.'.$format.'.twig' );
-
+        $response = new Response();
         // Set caching
         if ( $this->container->getParameter( 'kernel.environment') == 'prod' ) {
             $response->setMaxAge( 129600 );
             $response->setPublic();
         }
+        $response->setETag( $this->container->getParameter('asset_update') );
+        if ( $response->isNotModified( $request ) ) { return $response; }
 
-        return $response;
+        return $this->render( 'BluelinePagesBundle::'.$page.'.'.$format.'.twig', array(), $response );
     }
 }
