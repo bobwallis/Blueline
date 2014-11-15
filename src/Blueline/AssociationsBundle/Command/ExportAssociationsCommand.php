a42e5b01cd58925ae81cffd1fc08ba0650088a56
Bob Wallis <bob.wallis@gmail.com>
2014-11-15 17:46:57 +0000

    Fix associations import to work with differences between strcmp and how Postgres orders data

--- src/Blueline/AssociationsBundle/Command/ExportAssociationsCommand.php
+++ src/Blueline/AssociationsBundle/Command/ExportAssociationsCommand.php
@@ -17,7 +17,7 @@ class ExportAssociationsCommand extends ContainerAwareCommand
     {
         // Get an array of association data
         $associations = $this->getContainer()->get( 'doctrine' )->getManager()
-                             ->createQuery( 'SELECT a.abbreviation, a.name, a.link FROM BluelineAssociationsBundle:Association a ORDER BY a.abbreviation' )
+                             ->createQuery( 'SELECT a.abbreviation, a.name, a.link FROM BluelineAssociationsBundle:Association a ORDER BY a.abbreviation ASC' )
                              ->getArrayResult();
         // Print it
         $output->write( "<?php\n// Associations data exported ".date( 'Y/m/d, H:i' )."\n\$associations = " );
--- src/Blueline/AssociationsBundle/Command/ImportAssociationsCommand.php
+++ src/Blueline/AssociationsBundle/Command/ImportAssociationsCommand.php
@@ -30,63 +30,51 @@ class ImportAssociationsCommand extends ContainerAwareCommand
         $associations = new \ArrayObject( $associations );
 
         // Get access to the entity manager and validator service
-        $em        = $this->getContainer()->get( 'doctrine' )->getManager();
-        $validator = $this->getContainer()->get( 'validator' );
-        $progress  = $this->getHelperSet()->get('progress');
+        $em                    = $this->getContainer()->get( 'doctrine' )->getManager();
+        $associationRepository = $em->getRepository( 'BluelineAssociationsBundle:Association' );
+        $validator             = $this->getContainer()->get( 'validator' );
+        $progress              = $this->getHelperSet()->get('progress');
 
-        // Iterate over the data
-        $dbIterator  = $em->createQuery( 'SELECT a FROM Blueline\AssociationsBundle\Entity\Association a ORDER BY a.abbreviation ASC' )->iterate();
-        $dbIterator->next(); // For some reason the Doctrine query iterator needs ->next() called before it gives the first row
+        // Iterate over the data and import/update
         $txtIterator = $associations->getIterator();
+        $importedAssociations = array();
         $progress->start( $output, count($associations) );
         $progress->setBarWidth( $targetConsoleWidth - (strlen((string)count($associations))*2) - 10 );
         $progress->setRedrawFrequency( max(1,count($associations)/100) );
-        while ( $dbIterator->valid() || $txtIterator->valid() ) {
-            $dbRow  = $dbIterator->current();
+        while ( $txtIterator->valid() ) {
             $txtRow = $txtIterator->current();
-            $strcmp = ( $dbRow && $txtRow )? strcmp( $txtRow['abbreviation'], $dbRow[0]->getAbbreviation() ) : null;
-
-            // If we run out of text, or the abbreviation of the text row is greater than that of the database row,  delete any remaining database entries
-            if ( !$txtIterator->valid() || $strcmp > 0 ) {
-                $em->remove( $dbRow[0] );
-                $dbIterator->next();
-            }
-            // If we run out of database, or the abbreviation of the text row is less than that of the database row, import the text row
-            elseif ( !$dbIterator->valid() || $strcmp < 0 ) {
+            $importedAssociations[] = $txtRow['abbreviation'];
+            $association = $associationRepository->findOneByAbbreviation( $txtRow['abbreviation'] );
+            if( !$association ) {
                 $association = new Association();
-                $association->setAbbreviation( $txtRow['abbreviation'] );
-                $association->setName( $txtRow['name'] );
-                $association->setLink( $txtRow['link'] );
-                $errors = $validator->validate( $association );
-                if ( count( $errors ) > 0 ) {
-                    $progress->clear();
-                    $output->writeln( "\r<error> Invalid data for ".$txtRow['name'].":\n".$errors.'</error>' );
-                    $progress->display();
-                } else {
-                    $em->persist( $association );
-                }
-                $txtIterator->next();
-                $progress->advance();
             }
-            // If the abbreviations of the database row and the text row match, update the database row with the text one
-            elseif ( $dbRow[0]->getAbbreviation() == $txtRow['abbreviation'] ) {
-                $dbRow[0]->setName( $txtRow['name'] );
-                $dbRow[0]->setLink( $txtRow['link'] );
-                $errors = $validator->validate( $dbRow[0] );
-                if ( count( $errors ) > 0 ) {
-                    $progress->clear();
-                    $output->writeln( "\r<error> Invalid data for ".$txtRow['name'].":\n".$errors.'</error>' );
-                    $progress->display();
-                    $em->detach( $dbRow[0] );
-                }
-                $txtIterator->next();
-                $dbIterator->next();
-                $progress->advance();
+            $association->setAbbreviation( $txtRow['abbreviation'] );
+            $association->setName( $txtRow['name'] );
+            $association->setLink( $txtRow['link'] );
+            $errors = $validator->validate( $association );
+            if ( count( $errors ) > 0 ) {
+                $progress->clear();
+                $output->writeln( "\r<error> Invalid data for ".$txtRow['name'].":\n".$errors.'</error>' );
+                $progress->display();
+            } else {
+                $em->merge( $association );
             }
+            $txtIterator->next();
+            $progress->advance();
         }
         $progress->finish();
-        // Flush all changes to the database, and finish
         $em->flush();
-        $output->writeln( "\n<info>Finished updating associaton data.. Peak memory usage: ".number_format( memory_get_peak_usage() ).' bytes.</info>' );
+        $em->clear();
+
+        // Check for deletions
+        $abbreviationsInDatabase = $em->createQuery( 'SELECT a.abbreviation FROM Blueline\AssociationsBundle\Entity\Association a' )->getScalarResult();
+        foreach( $abbreviationsInDatabase as $a ) {
+            $a = current( $a );
+            if( !in_array( $a, $importedAssociations ) ) {
+                $em->createQuery('DELETE FROM Blueline\AssociationsBundle\Entity\Association a where a.abbreviation = :abbreviation')
+                    ->setParameter( 'abbreviation', $a )
+                    ->execute();
+            }
+        }
     }
 }
