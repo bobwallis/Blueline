81239909a6188e25d51bd1202aa94818ca9cbdb0
Bob Wallis <bob.wallis@gmail.com>
2014-12-01 20:43:11 +0000

    Comedy error

--- src/Blueline/MethodsBundle/Resources/public/js/ui/MethodView.js
+++ src/Blueline/MethodsBundle/Resources/public/js/ui/MethodView.js
@@ -10,7 +10,7 @@
 
 define( ['jquery', 'shared/lib/webfont!Blueline', '../helpers/Grid', '../helpers/PlaceNotation'], function( $, webFontLoaded, MethodGrid, PlaceNotation ) {
 	// Display messages if canvas is not supported
-	if( true||!Modernizr.canvas ) {
+	if( !Modernizr.canvas ) {
 		return function( options ) {
 			$( options.numbersContainer ).html( '<div class="wrap"><img src="'+location.href+'.png" /></div>' );
 			$( options.gridContainer ).html( '<div class="wrap"><p class="nothing">Your browser doesn\'t support canvas elements, and so can\'t draw methods. Consider upgrading to a more modern browser.</p></div>' );
