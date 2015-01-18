68e3536f8d5c4952f559620d88c19252831200df
Bob Wallis <bob.wallis@gmail.com>
2015-01-18 21:31:27 +0000

    Cache results of MeasureCanvasText

--- src/Blueline/BluelineBundle/Resources/public/js/helpers/MeasureCanvasText.js
+++ src/Blueline/BluelineBundle/Resources/public/js/helpers/MeasureCanvasText.js
@@ -1,10 +1,14 @@
-define( ['jquery', '../ui/Canvas'], function( $, Canvas ) {
-
+define( ['jquery', '../ui/Canvas', './LocalStorage'], function( $, Canvas, LocalStorage ) {
 	var measureText = function( text, font ) {
-		var testCanvas = $( '<canvas></canvas>' ).get( 0 ),
-			ctx = testCanvas.getContext( '2d' );
-		ctx.font = font;
-		return ctx.measureText( text );
+		var width = LocalStorage.getItem( 'Width.'+font+text );
+		if( width === null ) {
+			var testCanvas = $( '<canvas></canvas>' ).get( 0 ),
+				ctx = testCanvas.getContext( '2d' );
+			ctx.font = font;
+			width = ctx.measureText( text );
+			LocalStorage.setItem( 'Width.'+font+text, width );
+		}
+		return width;
 	};
 
 	return measureText;
