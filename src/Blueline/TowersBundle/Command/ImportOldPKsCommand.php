102e503550c553169b01cc06cc87ff31a928d98e
Bob Wallis <bob.wallis@gmail.com>
2015-02-01 10:27:29 +0000

    pg_* functions to import old primary key data

--- src/Blueline/TowersBundle/Command/ImportOldPKsCommand.php
+++ src/Blueline/TowersBundle/Command/ImportOldPKsCommand.php
@@ -1,20 +1,11 @@
 <?php
-/*
- * This file is part of Blueline.
- * It implements a Symfony command which parses the file newpks.txt and imports it into the
- * database.
- *
- * (c) Bob Wallis <bob.wallis@gmail.com>
- *
- */
-
 namespace Blueline\TowersBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
-use Blueline\TowersBundle\Entity\OldPK;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Blueline\TowersBundle\Helpers\OldPKTxtIterator;
 
 class ImportOldPKsCommand extends ContainerAwareCommand
@@ -35,35 +26,44 @@ class ImportOldPKsCommand extends ContainerAwareCommand
         // Print title
         $output->writeln('<title>Updating tower old primary key data</title>');
 
-        // Get entity manager, tower repository, and a progress bar
-        $em              = $this->getContainer()->get('doctrine')->getManager();
-        $towerRepository = $em->getRepository('BluelineTowersBundle:Tower');
-        $progress        = $this->getHelperSet()->get('progress');
+        // Get access to the database and other services
+        $db = pg_connect('host='.$this->getContainer()->getParameter('database_host').' port='.$this->getContainer()->getParameter('database_port').' dbname='.$this->getContainer()->getParameter('database_name').' user='.$this->getContainer()->getParameter('database_user').' password='.$this->getContainer()->getParameter('database_password').'');
+        if ($db === false) {
+            $output->writeln('<error>Failed to connect to database</error>');
+            return;
+        }
 
         // Delete all existing data
-        $em->createQuery('DELETE FROM BluelineTowersBundle:OldPK')->getResult();
-        $em->flush();
+        $output->writeln('<info>Clear existing oldpk data...</info>');
+        if (pg_query($db, 'DELETE FROM towers_oldpks') === false) {
+            $output->writeln('<error>Failed to clear existing data: '.pg_last_error($db).'</error>');
+            return;
+        }
 
-        // Iterate over newpks.txt, importing data
+        // Prepare a query for inserting records
+        if (pg_prepare($db, 'insertOldpk', 'INSERT INTO towers_oldpks (oldpk, tower_id) VALUES ($1, $2)') === false) {
+            $output->writeln('<error>Failed to create prepared query: '.pg_last_error($db).'</error>');
+            return;
+        }
+
+        // Create the iterator
         $txtIterator = new OldPKTxtIterator(__DIR__.'/../Resources/data/newpks.txt');
         $oldPKCount = count($txtIterator);
-        $progress->start($output, $oldPKCount);
+
+        // Set up the progress bar
+        $progress = new ProgressBar($output, $oldPKCount);
         $progress->setBarWidth($targetConsoleWidth - (strlen((string) $oldPKCount)*2) - 10);
         $progress->setRedrawFrequency($oldPKCount/100);
+        $progress->start();
+
+        // Iterate over newpks.txt, importing data
         while ($txtIterator->valid()) {
             $txtRow = $txtIterator->current();
-            // Try to find the tower that is being referenced
-            $tower = $towerRepository->findOneById($txtRow['tower_id']);
-            if (!$tower) {
+
+            if (@pg_execute($db, 'insertOldpk', array($txtRow['oldpk'], $txtRow['tower_id'])) === false) {
                 $progress->clear();
-                $output->writeln("\r<comment>".str_pad(" DoveID '".$txtRow['tower_id']."' is a target in newpks.txt, but isn't in the tower table", $targetConsoleWidth, ' ')."</comment>");
+                $output->writeln("\r<comment> DoveID '".$txtRow['tower_id']."' failed to insert: ".pg_last_error($db)."</comment>");
                 $progress->display();
-            } else {
-                // Create the OldPK object, and persist it
-                $oldpk = new OldPK();
-                $oldpk->setOldPK($txtRow['oldpk']);
-                $oldpk->setTower($tower);
-                $em->persist($oldpk);
             }
 
             $txtIterator->next();
@@ -71,8 +71,7 @@ class ImportOldPKsCommand extends ContainerAwareCommand
         }
         $progress->finish();
 
-        // Flush all changes to the database, and finish
-        $em->flush();
+        // Finish
         $output->writeln("\n<info>Finished updating old primary key data.. Peak memory usage: ".number_format(memory_get_peak_usage()).' bytes.</info>');
     }
 }
