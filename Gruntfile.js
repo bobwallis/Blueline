d172abe3fa1cdb9d0438d45f81f28b10752b7135
Bob Wallis <bob.wallis@gmail.com>
2015-02-28 22:23:12 +0000

    Grunt watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,66 @@
 module.exports = function (grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		watch: {
+			gif: {
+				files: ['src/Blueline/**/*.gif'],
+				tasks: ['copy:gif', 'imagemin:gif'],
+				options: { spawn: false }
+			},
+			svg: {
+				files: ['src/Blueline/**/*.svg'],
+				tasks: ['copy:svg', 'imagemin:svg', 'compress:svg', 'svg2png', 'imagemin:png'],
+				options: { spawn: false }
+			}
+		},
+
 		copy: {
 			fonts: {
 				files: [
 					{ expand: true, flatten: true, dest: 'web/fonts/', src: 'src/Blueline/**/fonts/*.{woff,ttf}' }
 				]
 			},
-			images: {
+			gif: {
 				files: [
-					{ expand: true, flatten: true, dest: 'web/images/', src: ['src/Blueline/**/images/*.{svg,gif}', '!**/{favicon,iosicon}.svg'] }
+					{ expand: true, flatten: true, dest: 'web/images/', src: ['src/Blueline/**/images/*.gif'] }
+				]
+			},
+			svg: {
+				files: [
+					{ expand: true, flatten: true, dest: 'web/images/', src: ['src/Blueline/**/images/*.svg', '!**/{favicon,iosicon}.svg'] }
 				]
 			}
 		},
+
 		svg2png: {
 			all: {
 				src: ['web/images/*.svg']
 			}
 		},
+
 		imagemin: {
 			all: {
 				files: [
 					{ expand: true, src: ['web/images/*'] }
 				]
+			},
+			gif: {
+				files: [
+					{ expand: true, src: ['web/images/*.gif'] }
+				]
+			},
+			png: {
+				files: [
+					{ expand: true, src: ['web/images/*.png'] }
+				]
+			},
+			svg: {
+				files: [
+					{ expand: true, src: ['web/images/*.svg'] }
+				]
 			}
 		},
+
 		compress: {
 			all: {
 				options: {
@@ -35,6 +71,36 @@ module.exports = function (grunt) {
 				files: [
 					{ expand: true, rename: function(d,s) { return s+'.gz'; },  src: ['web/**/*.{css,js,svg}'] }
 				]
+			},
+			css: {
+				options: {
+					mode: 'gzip',
+					pretty: true,
+					level: 6
+				},
+				files: [
+					{ expand: true, rename: function(d,s) { return s+'.gz'; },  src: ['web/**/*.css'] }
+				]
+			},
+			js: {
+				options: {
+					mode: 'gzip',
+					pretty: true,
+					level: 6
+				},
+				files: [
+					{ expand: true, rename: function(d,s) { return s+'.gz'; },  src: ['web/**/*.js'] }
+				]
+			},
+			svg: {
+				options: {
+					mode: 'gzip',
+					pretty: true,
+					level: 6
+				},
+				files: [
+					{ expand: true, rename: function(d,s) { return s+'.gz'; },  src: ['web/**/*.svg'] }
+				]
 			}
 		}
 	});
@@ -43,5 +109,7 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-svg2png');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-compress');
-	grunt.registerTask('default', 'Build all assets.', ['copy', 'svg2png', 'imagemin', 'compress']);
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	
+	grunt.registerTask('default', 'Build all assets.', ['copy:all', 'svg2png', 'imagemin:all', 'compress:all']);
 };
