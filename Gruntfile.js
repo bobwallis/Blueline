48cc6b2bf9a66377fcad8992793bb7c80c3bd771
Bob Wallis <bob.wallis@gmail.com>
2015-02-28 21:30:03 +0000

    Copy in and optimize images

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,13 +4,31 @@ module.exports = function (grunt) {
 		copy: {
 			fonts: {
 				files: [
-					{ dest: 'web/fonts/Blueline.woff', src: 'src/Blueline/BluelineBundle/Resources/public/fonts/Blueline.woff' },
-					{ dest: 'web/fonts/Blueline.ttf', src: 'src/Blueline/BluelineBundle/Resources/public/fonts/Blueline.ttf' }
+					{ expand: true, flatten: true, dest: 'web/fonts/', src: 'src/Blueline/**/fonts/*.{woff,ttf}' }
+				]
+			},
+			images: {
+				files: [
+					{ expand: true, flatten: true, dest: 'web/images/', src: ['src/Blueline/**/images/*.{svg,gif}', '!**/{favicon,iosicon}.svg'] }
+				]
+			}
+		},
+		svg2png: {
+			all: {
+				src: ['web/images/*.svg']
+			}
+		},
+		imagemin: {
+			all: {
+				files: [
+					{ expand: true, flatten: true, dest: 'web/images/', src: ['web/images/*'] }
 				]
 			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.registerTask('default', ['copy']);
+	grunt.loadNpmTasks('grunt-svg2png');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.registerTask('default', 'Build all assets.', ['copy', 'svg2png', 'imagemin']);
 };
