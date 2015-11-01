c206f07917371d38a38f03e0a91913cfbea44177
Bob Wallis <bob.wallis@gmail.com>
2015-11-01 10:51:56 +0000

    Transparency in rules-offs

--- src/Blueline/MethodsBundle/Resources/public/js/helpers/Grid/Options.js
+++ src/Blueline/MethodsBundle/Resources/public/js/helpers/Grid/Options.js
@@ -58,7 +58,7 @@ define( ['jquery', '../PlaceNotation', '../../../shared/helpers/MeasureCanvasTex
 		},
 		ruleOffs: {
 			show: true,
-			stroke: '#999',
+			stroke: 'rgba(0,0,0,0.4)',
 			dash:   [3,1],
 			width:  1,
 			cap:    'butt',
