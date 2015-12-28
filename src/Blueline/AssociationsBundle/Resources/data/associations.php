f804ed2c7b4cecee38b2b053738b74c511871f28
Bob Wallis <bob.wallis@gmail.com>
2015-12-28 23:29:35 +0000

    Remove outline field from associations

--- src/Blueline/AssociationsBundle/Resources/data/associations.php
+++ src/Blueline/AssociationsBundle/Resources/data/associations.php
@@ -6,412 +6,353 @@ $associations = array (
     'id' => 'ANZAB',
     'name' => 'Australian and New Zealand Association',
     'link' => 'http://www.anzab.org.au/',
-    'outline' => NULL,
   ),
   1 =>
   array (
     'id' => 'ASCY',
     'name' => 'Ancient Society of College Youths',
     'link' => 'http://www.ascy.org.uk/',
-    'outline' => NULL,
   ),
   2 =>
   array (
     'id' => 'Beds',
     'name' => 'Bedfordshire Association',
     'link' => 'http://www.bacr.co.uk/',
-    'outline' => NULL,
   ),
   3 =>
   array (
     'id' => 'Bev&D',
     'name' => 'Beverley & District Society',
     'link' => 'http://www.bnd.org.uk/',
-    'outline' => NULL,
   ),
   4 =>
   array (
     'id' => 'B&W',
     'name' => 'Bath and Wells Diocesan Association',
     'link' => 'http://bath-wells.org/',
-    'outline' => NULL,
   ),
   5 =>
   array (
     'id' => 'CarDG',
     'name' => 'Carlisle Diocesan Guild',
     'link' => 'http://www.carlisle-dgcbr.org.uk/',
-    'outline' => NULL,
   ),
   6 =>
   array (
     'id' => 'CheDG',
     'name' => 'Chester Diocesan Guild',
     'link' => 'http://www.chesterdg.org.uk/',
-    'outline' => NULL,
   ),
   7 =>
   array (
     'id' => 'CovDG',
     'name' => 'Coventry Diocesan Guild',
     'link' => 'http://www.coventrydg.co.uk/',
-    'outline' => NULL,
   ),
   8 =>
   array (
     'id' => 'CUG',
     'name' => 'Cambridge University Guild',
     'link' => 'http://www.cugcr.org.uk/',
-    'outline' => NULL,
   ),
   9 =>
   array (
     'id' => 'DDA',
     'name' => 'Derby Diocesan Association',
     'link' => 'http://www.derbyda.org.uk/',
-    'outline' => NULL,
   ),
   10 =>
   array (
     'id' => 'DevAs',
     'name' => 'Devon Association',
     'link' => 'http://www.devonbells.co.uk/',
-    'outline' => NULL,
   ),
   11 =>
   array (
     'id' => 'D&N',
     'name' => 'Durham and Newcastle Diocesan Association',
     'link' => 'http://dandn.org/',
-    'outline' => NULL,
   ),
   12 =>
   array (
     'id' => 'EDWNA',
     'name' => 'East Derbyshire and West Nottinghamshire Association',
     'link' => '',
-    'outline' => NULL,
   ),
   13 =>
   array (
     'id' => 'EGDG',
     'name' => 'East Grinstead and District Guild',
     'link' => 'http://www.egdg.org.uk/home.htm',
-    'outline' => NULL,
   ),
   14 =>
   array (
     'id' => 'Ely',
     'name' => 'Ely Diocesan Association',
     'link' => 'https://sites.google.com/site/elydabells/',
-    'outline' => NULL,
   ),
   15 =>
   array (
     'id' => 'Essex',
     'name' => 'Essex Association',
     'link' => 'http://www.eacr.org.uk/',
-    'outline' => NULL,
   ),
   16 =>
   array (
     'id' => 'G&B',
     'name' => 'Gloucester and Bristol Diocesan Association',
     'link' => 'http://www.bellsgandb.org.uk/',
-    'outline' => NULL,
   ),
   17 =>
   array (
     'id' => 'GDG',
     'name' => 'Guildford Diocesan Guild',
     'link' => 'http://guildfordguild.org.uk/',
-    'outline' => NULL,
   ),
   18 =>
   array (
     'id' => 'GDR',
     'name' => 'Guild of Devonshire Ringers',
     'link' => 'http://devonringers.org.uk/',
-    'outline' => NULL,
   ),
   19 =>
   array (
     'id' => 'HCA',
     'name' => 'Hertford County Association',
     'link' => 'http://www.hcacr.org.uk/',
-    'outline' => NULL,
   ),
   20 =>
   array (
     'id' => 'HDG',
     'name' => 'Hereford Diocesan Guild',
     'link' => 'http://www.hdgb.org/',
-    'outline' => NULL,
   ),
   21 =>
   array (
     'id' => 'Irish',
     'name' => 'Irish Association',
     'link' => 'http://www.bellringingireland.org/',
-    'outline' => NULL,
   ),
   22 =>
   array (
     'id' => 'KCA',
     'name' => 'Kent County Association',
     'link' => 'http://www.kcacr.org.uk/',
-    'outline' => NULL,
   ),
   23 =>
   array (
     'id' => 'Lancs',
     'name' => 'Lancashire Association',
     'link' => 'http://www.lacr.org.uk/',
-    'outline' => NULL,
   ),
   24 =>
   array (
     'id' => 'LeiDG',
     'name' => 'Leicester Diocesan Guild',
     'link' => 'http://www.leicesterdg.org.uk/',
-    'outline' => NULL,
   ),
   25 =>
   array (
     'id' => 'LinDG',
     'name' => 'Lincoln Diocesan Guild',
     'link' => 'http://ldgcb.org.uk/',
-    'outline' => NULL,
   ),
   26 =>
   array (
     'id' => 'LivUS',
     'name' => 'Liverpool Universities Society',
     'link' => 'http://luscr.org.uk/',
-    'outline' => NULL,
   ),
   27 =>
   array (
     'id' => 'L&M',
     'name' => 'Llandaff and Monmouth Diocesan Association',
     'link' => 'http://www.llanmon.org.uk/',
-    'outline' => NULL,
   ),
   28 =>
   array (
     'id' => 'Lundy',
     'name' => 'Lundy Island Society',
     'link' => '',
-    'outline' => NULL,
   ),
   29 =>
   array (
     'id' => 'LWAS',
     'name' => 'Lichfield and Walsall Archdeaconries Society',
     'link' => 'http://www.lwascr.org.uk/',
-    'outline' => NULL,
   ),
   30 =>
   array (
     'id' => 'Middx',
     'name' => 'Middlesex County Association',
     'link' => 'http://www.mcaldg.org.uk/',
-    'outline' => NULL,
   ),
   31 =>
   array (
     'id' => 'MUG',
     'name' => 'Manchester University Guild',
     'link' => 'http://www.mugcr.org/',
-    'outline' => NULL,
   ),
   32 =>
   array (
     'id' => 'NAG',
     'name' => 'North American Guild',
     'link' => 'http://www.nagcr.org/',
-    'outline' => NULL,
   ),
   33 =>
   array (
     'id' => 'NDA',
     'name' => 'Norwich Diocesan Association',
     'link' => 'http://ndar.org.uk/',
-    'outline' => NULL,
   ),
   34 =>
   array (
     'id' => 'NSA',
     'name' => 'North Staffordshire Association',
     'link' => 'http://www.nsacr.org.uk/',
-    'outline' => NULL,
   ),
   35 =>
   array (
     'id' => 'NWA',
     'name' => 'North Wales Asssociation',
     'link' => 'http://www.northwalesbellringers.org/',
-    'outline' => NULL,
   ),
   36 =>
   array (
     'id' => 'ODG',
     'name' => 'Oxford Diocesan Guild',
     'link' => 'http://www.odg.org.uk/',
-    'outline' => NULL,
   ),
   37 =>
   array (
     'id' => 'OS',
     'name' => 'Oxford Society',
     'link' => 'http://www.oxfordsociety.org.uk/',
-    'outline' => NULL,
   ),
   38 =>
   array (
     'id' => 'OUS',
     'name' => 'Oxford University Society',
     'link' => 'http://www.ouscr.org.uk/',
-    'outline' => NULL,
   ),
   39 =>
   array (
     'id' => 'PDG',
     'name' => 'Peterborough Diocesan Guild',
     'link' => 'http://www.pdg.org.uk/',
-    'outline' => NULL,
   ),
   40 =>
   array (
     'id' => 'SAG',
     'name' => 'South African Guild',
     'link' => 'http://www.scifac.ru.ac.za/cathedral/bellguild.htm',
-    'outline' => NULL,
   ),
   41 =>
   array (
     'id' => 'Salis',
     'name' => 'Salisbury Diocesan Guild',
     'link' => 'http://sdgr.org.uk/',
-    'outline' => NULL,
   ),
   42 =>
   array (
     'id' => 'Salop',
     'name' => 'Shropshire Association',
     'link' => 'http://www.sacbr.org.uk/',
-    'outline' => NULL,
   ),
   43 =>
   array (
     'id' => 'S&B',
     'name' => 'Swansea and Brecon Diocesan Guild',
     'link' => 'http://myweb.tiscali.co.uk/pckmj/sbdg/',
-    'outline' => NULL,
   ),
   44 =>
   array (
     'id' => 'Scot',
     'name' => 'Scottish Association',
     'link' => 'http://www.sacr.org/',
-    'outline' => NULL,
   ),
   45 =>
   array (
     'id' => 'SDDG',
     'name' => 'St David\'s Diocesan Guild',
     'link' => 'https://sites.google.com/site/stdavidsguild/home',
-    'outline' => NULL,
   ),
   46 =>
   array (
     'id' => 'SMB',
     'name' => 'St Martin\'s Guild',
     'link' => 'http://www.smgcbr.heralded.co.uk/',
-    'outline' => NULL,
   ),
   47 =>
   array (
     'id' => 'SRCY',
     'name' => 'Society of Royal Cumberland Youths',
     'link' => 'http://www.srcy.org.uk/',
-    'outline' => NULL,
   ),
   48 =>
   array (
     'id' => 'Suff',
     'name' => 'Suffolk Guild',
     'link' => 'http://www.suffolkbells.org.uk/',
-    'outline' => NULL,
   ),
   49 =>
   array (
     'id' => 'Surr',
     'name' => 'Surrey Association',
     'link' => 'http://www.surreybellringers.org.uk/',
-    'outline' => NULL,
   ),
   50 =>
   array (
     'id' => 'SuxCA',
     'name' => 'Sussex County Association',
     'link' => 'http://www.scacr.org/',
-    'outline' => NULL,
   ),
   51 =>
   array (
     'id' => 'Swell',
     'name' => 'Southwell and Nottingham Diocesan Guild',
     'link' => 'http://www.southwelldg.org.uk/',
-    'outline' => NULL,
   ),
   52 =>
   array (
     'id' => 'TruDG',
     'name' => 'Truro Diocesan Guild',
     'link' => 'http://www.tdgr.org.uk/',
-    'outline' => NULL,
   ),
   53 =>
   array (
     'id' => 'UBSCR',
     'name' => 'University of Bristol Society',
     'link' => 'http://www.bris.ac.uk/Depts/Union/UBSCR/',
-    'outline' => NULL,
   ),
   54 =>
   array (
     'id' => 'ULSCR',
     'name' => 'University of London Society',
     'link' => 'http://www.ulscr.org.uk/',
-    'outline' => NULL,
   ),
   55 =>
   array (
     'id' => 'WDA',
     'name' => 'Worcestershire & Districts Association',
     'link' => 'http://www.wdcra.org.uk/',
-    'outline' => NULL,
   ),
   56 =>
   array (
     'id' => 'W&P',
     'name' => 'Winchester and Portsmouth Diocesan Guild',
     'link' => 'http://www.wp-ringers.org.uk/',
-    'outline' => NULL,
   ),
   57 =>
   array (
     'id' => 'YACR',
     'name' => 'Yorkshire Association',
     'link' => 'http://www.yacr.org.uk/',
-    'outline' => NULL,
   ),
   58 =>
   array (
     'id' => 'Zimb',
     'name' => 'Zimbabwe Guild',
     'link' => '',
-    'outline' => NULL,
   ),
 );
