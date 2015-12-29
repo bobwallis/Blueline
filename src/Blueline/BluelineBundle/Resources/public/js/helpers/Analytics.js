a6f928300862e53cec155f34cc74f562c0ebb236
Bob Wallis <bob.wallis@gmail.com>
2015-12-29 20:56:17 +0000

    Use anonymizeIp feature

--- src/Blueline/BluelineBundle/Resources/public/js/helpers/Analytics.js
+++ src/Blueline/BluelineBundle/Resources/public/js/helpers/Analytics.js
@@ -1,6 +1,7 @@
 define( ['eve'], function( eve ) {
 	eve.on( 'page.loaded', function() {
 		if( typeof ga === 'function' ) {
+			ga( 'set', 'anonymizeIp', true );
 			ga( 'send', 'pageview' );
 		}
 	} );
