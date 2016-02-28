ebce986ca2d58968dc16292531b80b45dab96a98
Bob Wallis <bob.wallis@gmail.com>
2016-02-28 20:58:25 +0000

    Tweaks to analytics

--- src/Blueline/BluelineBundle/Resources/public/js/helpers/Analytics.js
+++ src/Blueline/BluelineBundle/Resources/public/js/helpers/Analytics.js
@@ -1,8 +1,24 @@
-define( ['eve'], function( eve ) {
-	eve.on( 'page.loaded', function() {
+define( ['jquery', 'eve', '../helpers/URL'], function( $, eve, URL ) {
+	// Set options
+	if( typeof ga === 'function' ) {
+		ga( 'set', 'anonymizeIp', true );
+	}
+
+	// Track page views using History API
+	eve.on( 'page.finished', function() {
 		if( typeof ga === 'function' ) {
-			ga( 'set', 'anonymizeIp', true );
 			ga( 'send', 'pageview' );
+			ga( 'set', { page: location.pathname + location.search, title: document.title } );
+		}
+	} );
+
+	// Track outbound links
+	$( document.body ).on( 'click', 'a', function( e ) {
+		if( typeof ga === 'function' ) {
+			var $target = $( e.target ).closest( 'a' );
+			if( $target.length > 0 && $target[0].hostname !== location.hostname ) {
+				ga( 'send', 'event', 'outbound', 'click', $target[0].href, { transport: 'beacon' } );
+			}
 		}
 	} );
 } );
