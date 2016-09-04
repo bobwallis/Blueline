d375144a2cf55c8585ef545beeb3184ec1b57cc5
Bob Wallis <bob.wallis@gmail.com>
2016-09-04 19:18:18 +0100

    Try to log JS exceptions to analytics provider

--- src/Blueline/BluelineBundle/Resources/public/js/helpers/Analytics.js
+++ src/Blueline/BluelineBundle/Resources/public/js/helpers/Analytics.js
@@ -1,7 +1,32 @@
 define( ['jquery', 'eve', '../helpers/URL'], function( $, eve, URL ) {
-	// Set options
 	if( typeof ga === 'function' ) {
+		// Set options
 		ga( 'set', 'anonymizeIp', true );
+
+		// JS exception tracking
+		window.onerror = function( msg, url, lineNo, columnNo, error ) {
+			try {
+				if( msg.toLowerCase().indexOf( 'script error' ) > -1 ){
+					return;
+				}
+				else {
+					var message = [
+						'JS Error',
+						'Message: ' + msg,
+						'URL: ' + url,
+						'Line: ' + lineNo,
+						'Column: ' + columnNo,
+						'Error object: ' + JSON.stringify(error)
+					].join( ' - ' );
+					ga( 'send', 'exception', {
+						'exDescription': message,
+						'exFatal': false
+					} );
+				}
+			} catch(e) {;}
+			return false;
+		};
+
 	}
 
 	// Track page views using History API
