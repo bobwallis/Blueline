cff0526576785f6ca44093b621247a140763115f
Bob Wallis <bob.wallis@gmail.com>
2016-09-07 00:16:20 +0100

    Replace IndexedDB library completely with own code

--- src/Blueline/BluelineBundle/Resources/public/js/data/Page/Cache/IndexedDB.js
+++ src/Blueline/BluelineBundle/Resources/public/js/data/Page/Cache/IndexedDB.js
@@ -1,11 +1,10 @@
-define( ['Modernizr', 'db', './Null', '../../../helpers/LocalStorage'], function( Modernizr, db, Null, LocalStorage ) {
+define( ['Modernizr', './Null', '../../../helpers/LocalStorage'], function( Modernizr, Null, LocalStorage ) {
 	var IndexedDB;
 	if( Modernizr.indexeddb ) {
-		// The IndexedDB API is a bit of a moving target. Once it stablises it would be sensible to trim
-		// this down a bit by using the actual API, but for now we'll just throw a wrapper over the top
-		// of it.
-		var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.oIndexedDB || window.msIndexedDB,
-			emptyFunction = function(){};
+		// Use prefixed indexDB if needed
+		window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+		window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+		window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
 
 		// Function to revert the cache back to the null version (use if setup fails)
 		var unsetup = function() {
@@ -13,52 +12,64 @@ define( ['Modernizr', 'db', './Null', '../../../helpers/LocalStorage'], function
 		};
 		unsetup();
 
-		db.open( {
-			server: 'Blueline',
-			version: 4,
-			schema: {
-				pages: {
-					key: {
-						keyPath: 'url'
-					},
-					indexes: {
-						timestamp: {
-							key: 'timestamp'
-						}
-					}
-				}
-			}
-		} )
-		.then( function ( server ) {
-			IndexedDB = { works: true };
+		var DBOpenRequest = window.indexedDB.open( 'Blueline', 5 );
+
+		DBOpenRequest.onerror = unsetup;
+
+		DBOpenRequest.onupgradeneeded = function( e ) {
+			var db = e.target.result;
+			db.onerror = unsetup;
+			db.deleteObjectStore( 'pages' );
+			var objectStore = db.createObjectStore( 'pages', { keyPath: 'url' } );
+			objectStore.createIndex( 'timestamp', 'timestamp', { unique: false });
+		};
+
+		DBOpenRequest.onsuccess = function() {
+			var db = DBOpenRequest.result;
+
+			IndexedDB.works = true;
 			IndexedDB.get = function( url, success, failure ) {
-				success = (typeof success === 'undefined') ? emptyFunction : success;
-				failure = (typeof failure === 'undefined') ? emptyFunction : failure;
-				server.get( 'pages', url )
-					.done( function( page ) {
-						if( typeof page === 'undefined' ) { failure(); }
-						else { success( page.content ); }
-					}, failure );
+				try {
+					var transaction = db.transaction( ['pages'], 'readonly' );
+					if( typeof failure === 'function' ) { transaction.onerror = failure; }
+					var objectStore = transaction.objectStore( 'pages' );
+					var objectStoreRequest = objectStore.get( url );
+					if( typeof failure === 'function' ) { objectStoreRequest.onerror = failure; }
+					if( typeof success === 'function' ) { objectStoreRequest.onsuccess = function() { (typeof objectStoreRequest.result === 'undefined')? failure() : success( objectStoreRequest.result.content ); }; }
+				} catch(e) { if( typeof failure === 'function' ) { failure(); } }
 			};
+
 			IndexedDB.set = function( url, content, success, failure ) {
-				success = (typeof success === 'undefined') ? emptyFunction : success;
-				failure = (typeof failure === 'undefined') ? emptyFunction : failure;
-				server.put( 'pages', { url: url, content: content, timestamp: Date.now() } ).then( success, failure );
+				try {
+					var transaction = db.transaction( ['pages'], 'readwrite' );
+					if( typeof failure === 'function' ) { transaction.onerror = failure; }
+					var objectStore = transaction.objectStore( 'pages' );
+					var objectStoreRequest = objectStore.put( { url: url, content: content, timestamp: Date.now() } );
+					if( typeof failure === 'function' ) { objectStoreRequest.onerror = failure; }
+					if( typeof success === 'function' ) { objectStoreRequest.onsuccess = success; }
+				} catch(e) { console.log('caught'); if( typeof failure === 'function' ) { failure(); } }
 			};
+
 			IndexedDB.remove = function( url, success, failure ) {
-				success = (typeof success === 'undefined') ? emptyFunction : success;
-				failure = (typeof failure === 'undefined') ? emptyFunction : failure;
-				server.remove( 'pages', url ).then( success, failure );
+				try {
+					var transaction = db.transaction( ['pages'], 'readwrite' );
+					if( typeof failure === 'function' ) { transaction.onerror = failure; }
+					var objectStore = transaction.objectStore( 'pages' );
+					var objectStoreRequest = objectStore.delete( url );
+					if( typeof failure === 'function' ) { objectStoreRequest.onerror = failure; }
+					if( typeof success === 'function' ) { objectStoreRequest.onsuccess = success; }
+				} catch(e) { if( typeof failure === 'function' ) { failure(); } }
 			};
+
 			IndexedDB.clear = function( success, failure ) {
-				success = (typeof success === 'undefined') ? emptyFunction : success;
-				failure = (typeof failure === 'undefined') ? emptyFunction : failure;
-				server.query( 'pages' ).all().execute()
-					.then( function( results ) {
-						results.forEach( function( e, i ) {
-							server.remove( 'pages', e.url );
-						} );
-					} );
+				try {
+					var transaction = db.transaction( ['pages'], 'readwrite' );
+					if( typeof failure === 'function' ) { transaction.onerror = failure; }
+					var objectStore = transaction.objectStore( 'pages' );
+					var objectStoreRequest = objectStore.clear();
+					if( typeof failure === 'function' ) { objectStoreRequest.onerror = failure; }
+					if( typeof success === 'function' ) { objectStoreRequest.onsuccess = success; }
+				} catch(e) { if( typeof failure === 'function' ) { failure(); } }
 			};
 
 			// Do an initial clear of the cache if needed
@@ -68,7 +79,7 @@ define( ['Modernizr', 'db', './Null', '../../../helpers/LocalStorage'], function
 				IndexedDB.clear();
 				LocalStorage.setItem( 'dbAge', LocalStorage.age );
 			}
-		} );
+		};
 	}
 	return IndexedDB;
 
