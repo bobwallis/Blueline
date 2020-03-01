ae090d4676665c261ea096b09a9d454ee505bd41
Bob Wallis <bob.wallis@gmail.com>
2020-03-01 14:19:56 +0000

    Remove jQuery from method tooltip, and generally improve it

--- src/Blueline/MethodsBundle/Resources/public/js/ui/MethodView/InteractiveGridOverlay.js
+++ src/Blueline/MethodsBundle/Resources/public/js/ui/MethodView/InteractiveGridOverlay.js
@@ -1,31 +1,32 @@
-define( ['jquery', 'eve'], function( $, eve ) {
-	// Create elements used for display
-	var $tooltip = $( '<div id="lineOverlay_tooltip"></div>' ).appendTo('body'),
-		$highlighter = $( '<div id="lineOverlay_highlight"/>' ).appendTo('body');
+define( ['eve'], function( eve ) {
+	// Create tooltip
+	var tooltipElement = document.createElement( 'div' );
+	tooltipElement.id = 'lineOverlay_tooltip';
+	document.body.appendChild( tooltipElement );
 
 	// Clear and reset if the page changes
 	eve.on( 'page.request', function() {
-		$tooltip.hide();
-		$highlighter.hide();
+		tooltipElement.style.display = 'none';
 	} );
 
-	var posToRow = function( x, y, gridOptions ) {
-		var row = gridOptions.dimensions.canvas.padding.top;
-		x -= gridOptions.dimensions.canvas.padding.left;
-	};
-
 	return function( grid, method, methodTexts ) {
+		// Don't do tooltips for hunt bells, filter them off
 		methodTexts = methodTexts.filter( function( m ) { return !m.hunt; } );
+		// For now just fail for differentials
 		if( methodTexts.length !== 1 ) { return; }
-		var $grid = $( '#'+grid.getOptions().id ),
-			$gridOffset = $grid.offset(),
+		// get some reusable sizing/positioning variables.
+		var gridElement = document.getElementById( grid.getOptions().id ),
+			gridRect = gridElement.getBoundingClientRect(),
 			gridOptions = grid.getOptions(),
-			topPadding = $gridOffset.top + gridOptions.dimensions.canvas.padding.top,
-			leftPadding = $gridOffset.left + gridOptions.dimensions.canvas.padding.left,
+			topPadding = gridRect.top + document.body.scrollTop + gridOptions.dimensions.canvas.padding.top,
+			leftPadding = gridRect.left + document.body.scrollLeft + gridOptions.dimensions.canvas.padding.left,
 			rowHeight = gridOptions.dimensions.row.height,
 			columnWidth = (gridOptions.dimensions.canvas.width - gridOptions.dimensions.canvas.padding.left) / gridOptions.layout.numberOfColumns,
+			// Use this to track whether or not we need to change what is displayed in the tooltip in this event loop tick
 			lastRow = -1,
+			// Use this to tell if the text is for places or points (we'll display the tooltip for those across a bigger area then they actually take up)
 			placePointRegex = /^(Make|Lead|Lie|Point)/,
+			// Format the text for display: Don't bother to show anything for hunting, and <sup> ordinal abbreviations
 			textToDisplay = methodTexts[0].text.coreRows.map( function( e ) {
 				if( e === 'Hunt up' || e === 'Hunt down') {
 					return null;
@@ -34,36 +35,43 @@ define( ['jquery', 'eve'], function( $, eve ) {
 					return e.replace( /(nds|rds|ths)/g, '<sup>$1</sup>' );
 				}
 			} );
-		$grid.off( 'mousemove mouseout mouseover')
-			.on( 'mousemove', function( e ) {
-				var row = Math.round( (e.pageY - topPadding) / rowHeight ) + (gridOptions.layout.changesPerColumn * Math.floor( (e.pageX - leftPadding) / columnWidth )) - 1;
-				if( row !== lastRow ) {
-					if( row < textToDisplay.length && textToDisplay[row] !== null ) {
-						$tooltip.css( { top: e.pageY - 5, left: e.pageX + 25, opacity: 1 } ).html( textToDisplay[row] );
-					}
-					// Allow places and points to overflow slightly outside the actual change where they happen
-					else if( row + 1 < textToDisplay.length && textToDisplay[row+1] !== null  && textToDisplay[row+1].search( placePointRegex ) !== -1 ) {
-						$tooltip.css( { top: e.pageY - 5, left: e.pageX + 25, opacity: 1 } ).html( textToDisplay[row+1] );
-					}
-					else if( typeof textToDisplay[row-1] !== 'undefined' && textToDisplay[row-1] !== null && textToDisplay[row-1].search( placePointRegex ) !== -1 ) {
-						$tooltip.css( { top: e.pageY - 5, left: e.pageX + 25, opacity: 1 } ).html( textToDisplay[row-1] );
-					}
-					else {
-						$tooltip.css( 'opacity', 0 );
-					}
-					lastRow = row;
+
+		gridElement.addEventListener( 'mousemove', function( e ) {
+			// Work out which row we're in
+			var row = Math.round( (e.pageY - topPadding) / rowHeight ) + (gridOptions.layout.changesPerColumn * Math.floor( (e.pageX - leftPadding) / columnWidth )) - 1;
+			// If we need to change the text then change it
+			if( row !== lastRow ) {
+				if( row < textToDisplay.length && textToDisplay[row] !== null ) {
+					tooltipElement.style.opacity = 1;
+					tooltipElement.innerHTML = textToDisplay[row];
 				}
+				// Allow places and points to overflow slightly outside the actual change where they happen
+				else if( row + 1 < textToDisplay.length && textToDisplay[row+1] !== null  && textToDisplay[row+1].search( placePointRegex ) !== -1 ) {
+					tooltipElement.style.opacity = 1;
+					tooltipElement.innerHTML = textToDisplay[row+1];
+				}
+				else if( typeof textToDisplay[row-1] !== 'undefined' && textToDisplay[row-1] !== null && textToDisplay[row-1].search( placePointRegex ) !== -1 ) {
+					tooltipElement.style.opacity = 1;
+					tooltipElement.innerHTML = textToDisplay[row-1];
+				}
+				// If nothing to display, then hide the tooltip
 				else {
-					$tooltip.css( { top: e.pageY - 5, left: e.pageX + 25 } );
+					tooltipElement.style.opacity = 0;
 				}
-			} )
-			.on( 'mouseout', function( e ) {
-				lastRow = -1;
-				$tooltip.css( 'opacity', 0 );
-			} )
-			.on( 'mouseover', function( e ) {
-				$tooltip.show();
-			} );
+				lastRow = row;
+			}
+			// Move the tooltip
+			tooltipElement.style.top = (e.pageY - 5)+'px';
+			console.log("offset 20");
+			tooltipElement.style.left = (e.pageX + 20)+'px';
+		} );
+		gridElement.addEventListener( 'mouseout', function( e ) {
+			lastRow = -1;
+			tooltipElement.style.opacity = 0;
+		} );
+		gridElement.addEventListener( 'mouseover', function( e ) {
+			tooltipElement.style.display = 'block';
+		} );
 	};
 
 } );
