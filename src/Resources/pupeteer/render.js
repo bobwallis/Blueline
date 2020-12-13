36a70bf680a31c0a0f855dfd4c3550fdcd45290f
Bob Wallis <bob.wallis@gmail.com>
2020-12-13 01:12:20 +0000

    Make sure browser always gets closed

--- src/Resources/pupeteer/render.js
+++ src/Resources/pupeteer/render.js
@@ -12,58 +12,67 @@ const scale = Math.round( parseFloat( process.argv[4] ) * 100 ) / 100,
 // Launch page
 const puppeteer = require( 'puppeteer' );
 ( async () => {
-    const browser = await puppeteer.launch( {
-        args: ['--no-sandbox'],
-        timeout: 10000,
-        ignoreHTTPSErrors: true
-    } );
-    const page = await browser.newPage();
-    await page.setViewport( { width: 5000, height: 1200, deviceScaleFactor: scale } );
-    await page.goto( url, { waitUntil: 'networkidle2' } );
-    const session = await page.target().createCDPSession();
-    await session.send( 'Emulation.setPageScaleFactor', {
-        pageScaleFactor: scale
-    } );
-
-    // Prepare page for the screenshot
-    await page.evaluate( (container) => {
-		// Remove elements not needed
-		var elements = document.querySelectorAll( '#top, #search, #loading, #menu, .method header, .method .details, .sf-toolbar' );
-		Array.prototype.forEach.call( elements, function( node ) {
-			node.parentNode.removeChild( node );
+    try {
+        const browser = await puppeteer.launch( {
+            args: ['--no-sandbox'],
+            timeout: 10000,
+            ignoreHTTPSErrors: true
         } );
-		// Select the right container
-		['line', 'grid'].forEach( function( e ) {
-			var elements = document.querySelectorAll( '.method .'+e );
-			Array.prototype.forEach.call( elements, function( node ) {
-				if( e == container ) {
-					node.style.display = 'block';
-				}
-				else {
-					node.parentNode.removeChild( node );
-				}
-			} );
-		} );
-		// Remove all margins and padding
-		var sheet = window.document.styleSheets[0];
-		sheet.insertRule( '* { margin: 0 !important; padding: 0 !important; }', sheet.cssRules.length);
-		sheet.insertRule( '.method .line canvas, .method .grid canvas { margin: 5px 20px 0 5px !important; padding: 0 0 5px 0 !important; }', sheet.cssRules.length);
-		sheet.insertRule( '.method .line canvas:first-child, .method .grid canvas:first-child { margin-right: 10px !important; }', sheet.cssRules.length);
-		sheet.insertRule( '.method .line canvas:last-child, .method .grid canvas:last-child { margin-right: 0 !important; padding-right: 5px !important; }', sheet.cssRules.length);
-    }, container );
+        const page = await browser.newPage();
+        await page.setViewport( { width: 5000, height: 1200, deviceScaleFactor: scale } );
+        await page.goto( url, { waitUntil: 'networkidle2' } );
+        const session = await page.target().createCDPSession();
+        await session.send( 'Emulation.setPageScaleFactor', {
+            pageScaleFactor: scale
+        } );
 
-    // Find the clip size
-    const dimensions = await page.evaluate( (container) => {
-        return {
-            x: 0,
-            y: 0,
-            width: $('.method .'+container+' canvas').map( function(i,e) { return $(e).outerWidth( true ); } ).toArray().reduce( function( prev, cur ) { return prev + cur; }, 0 ),
-            height: $('.method .'+container+' canvas:first-child').outerHeight( true )
-        };
-    }, container );
+        // Prepare page for the screenshot
+        await page.evaluate( (container) => {
+            // Remove elements not needed
+            var elements = document.querySelectorAll( '#top, #search, #loading, #menu, .method header, .method .details, .sf-toolbar' );
+            Array.prototype.forEach.call( elements, function( node ) {
+                node.parentNode.removeChild( node );
+            } );
+            // Select the right container
+            ['line', 'grid'].forEach( function( e ) {
+                var elements = document.querySelectorAll( '.method .'+e );
+                Array.prototype.forEach.call( elements, function( node ) {
+                    if( e == container ) {
+                        node.style.display = 'block';
+                    }
+                    else {
+                        node.parentNode.removeChild( node );
+                    }
+                } );
+            } );
+            // Remove all margins and padding
+            var sheet = window.document.styleSheets[0];
+            sheet.insertRule( '* { margin: 0 !important; padding: 0 !important; }', sheet.cssRules.length);
+            sheet.insertRule( '.method .line canvas, .method .grid canvas { margin: 5px 20px 0 5px !important; padding: 0 0 5px 0 !important; }', sheet.cssRules.length);
+            sheet.insertRule( '.method .line canvas:first-child, .method .grid canvas:first-child { margin-right: 10px !important; }', sheet.cssRules.length);
+            sheet.insertRule( '.method .line canvas:last-child, .method .grid canvas:last-child { margin-right: 0 !important; padding-right: 5px !important; }', sheet.cssRules.length);
+        }, container );
 
-    // Take and print screenshot, then close
-    var screenshot = await page.screenshot( { clip: dimensions } );
-    process.stdout.write( screenshot );
-    await browser.close();
+        // Find the clip size
+        const dimensions = await page.evaluate( (container) => {
+            return {
+                x: 0,
+                y: 0,
+                width: $('.method .'+container+' canvas').map( function(i,e) { return $(e).outerWidth( true ); } ).toArray().reduce( function( prev, cur ) { return prev + cur; }, 0 ),
+                height: $('.method .'+container+' canvas:first-child').outerHeight( true )
+            };
+        }, container );
+
+        // Take and print screenshot, then close
+        var screenshot = await page.screenshot( { clip: dimensions } );
+        process.stdout.write( screenshot );
+        await browser.close();
+    }
+    catch (error) {
+        console.log(error);
+        await browser.close();
+    }
+    finally {
+        await browser.close();
+    }
 } )();
