d897cf832517d79e15207ba88454f5acd7b8ae2a
Bob Wallis <bob.wallis@gmail.com>
2021-03-07 11:40:34 +0000

    Force an instant refresh if the service worker's changed and we're in a dev environment

--- src/Resources/js/helpers/ServiceWorker.js
+++ src/Resources/js/helpers/ServiceWorker.js
@@ -1,9 +1,26 @@
 define( ['./URL'], function( URL ) {
+    var dataAge = document.getElementsByTagName('html')[0].getAttribute( 'data-age' );
     return {
         load: function() {
             if( 'serviceWorker' in navigator ) {
                 navigator.serviceWorker
                     .register( URL.baseURL+'service_worker.js?base='+encodeURIComponent(URL.baseURL) )
+                    .then( function( registration ) {
+                        // Force an instant refresh if the service worker's changed and we're in a dev environment
+                        if( dataAge === 'dev' ) {
+                            registration.addEventListener( 'updatefound', function() {
+                                newWorker = registration.installing;
+                                newWorker.addEventListener( 'statechange', function() {
+                                    switch( newWorker.state ) {
+                                        case 'installed':
+                                            newWorker.postMessage( { action: 'skipWaiting' } );
+                                            window.location.reload();
+                                            break;
+                                    }
+                                } );
+                            } );
+                        }
+                    } );
             }
         },
         prefetch: function( url ) {
