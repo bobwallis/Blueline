38d7e2ba691df4bea7eaa5b3a4a8926e09cf2094
Bob Wallis <bob.wallis@gmail.com>
2022-05-11 20:23:22 +0000

    Remove jQuery

--- src/Resources/js/lib/webfont.js
+++ src/Resources/js/lib/webfont.js
@@ -1,7 +1,7 @@
 /*
  * Returns a function that tries to load the Blueline web font and then runs a callback whether it failed or not
  */
-define( ['jquery', 'eve'], function( $, eve ) {
+define( ['eve'], function( eve ) {
 	var loaded = false,
 		loading = false;
 
@@ -25,39 +25,42 @@ define( ['jquery', 'eve'], function( $, eve ) {
 			if( navigator.userAgent.toLowerCase().indexOf( 'android' ) !== -1 ) {
 				load();
 			}
+			// Use the CSS Font Loading Module if it's defined
 			else if( !!window.FontFace ) {
-				// Use the CSS Font Loading Module if it's defined
 				document.fonts.forEach( function(e) {
 					if( e.family == 'Blueline' || e.family == '"Blueline"' ) {
 						e.load().then( load, load );
 					}
 				} );
 			}
+			// Otherwise, compare the width of a test string in a sans font against the test string
+			// using the custom font, and wait for them to be different
 			else {
-				// Otherwise, compare the width of a test string in a sans font against the test string
-				// using the custom font, and wait for them to be different
-				var $body = $( document.body ),
-					calls = 0,
+				var calls = 0,
 					testAgainstFont = "arial,'URW Gothic L',sans-serif",
 					differenceLimit = 20, // WebKit seems to give slightly different widths even if the font hasn't loaded. Compensate by only confirming load if the difference is large
-					measureFont = function( $, $container, family ) {
-						var $testContainer = $( '<div class="fontPreload" style="font-family:' + family + '">BES</div>' ), width;
-						$container.append( $testContainer );
-						width = $testContainer.width();
-						$testContainer.remove();
+					measureFont = function( container, family ) {
+						var width,
+							testEl = document.createElement('div');
+						testEl.innerHTML = 'BES';
+						testEl.className = 'fontPreload';
+						testEl.style.fontFamily = family;
+						container.appendChild( testEl );
+						width = parseFloat( getComputedStyle( testEl, null ).width.replace( 'px', '' ) );
+						container.removeChild( testEl );
 						return width;
 					},
-					testAgainst = measureFont( $, $body, testAgainstFont ),
+					testAgainst = measureFont( document.body, testAgainstFont ),
 					checkIfLoaded = function() {
-						if( Math.abs( measureFont( $, $body, 'Blueline,'+testAgainstFont ) - testAgainst ) > differenceLimit ) {
+						if( Math.abs( measureFont( document.body, 'Blueline,'+testAgainstFont ) - testAgainst ) > differenceLimit ) {
 							load();
 							return;
 						}
-						if( ++calls > 20 ) {
+						if( ++calls > 25 ) {
 							load();
 						}
 						else {
-							setTimeout( checkIfLoaded, 100 );
+							setTimeout( checkIfLoaded, 150 );
 						}
 					};
 				checkIfLoaded();
