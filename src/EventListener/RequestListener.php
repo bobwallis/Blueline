02f9287f7c42746e71498aa75dde4c99c2ccf76a
Bob Wallis <bob.wallis@gmail.com>
2022-10-02 18:30:09 +0000

    Tidy

--- src/EventListener/RequestListener.php
+++ src/EventListener/RequestListener.php
@@ -17,20 +17,18 @@ class RequestListener
     public function onKernelRequest(RequestEvent $event)
     {
         // Add some missing mime-types
-        $event->getRequest()->setFormat('atom', 'application/atom+xml');
         $event->getRequest()->setFormat('jsonp', 'application/javascript');
         $event->getRequest()->setFormat('manifest', 'text/cache-manifest');
         $event->getRequest()->setFormat('ico', 'image/x-icon');
         $event->getRequest()->setFormat('gif', 'image/gif');
         $event->getRequest()->setFormat('png', 'image/png');
         $event->getRequest()->setFormat('jpg', 'image/jpg');
-        $event->getRequest()->setFormat('bmp', 'image/bmp');
         $event->getRequest()->setFormat('svg', 'image/svg+xml');
         $event->getRequest()->setFormat('pdf', 'application/pdf');
         $event->getRequest()->setFormat('woff', 'application/font-woff');
         $event->getRequest()->setFormat('woff2', 'application/font-woff2');
 
-        // Set global parameters that can be used in @Cache annotations and other placed
+        // Set global parameters that can be used in @Cache annotations and other places
         $event->getRequest()->attributes->set('endpoint', $this->container->getParameter('blueline.endpoint'));
         if ($this->container->getParameter('kernel.environment') == 'prod') {
             $event->getRequest()->attributes->set('asset_update', new \DateTime('@'.$this->container->getParameter('blueline.asset_update')));
