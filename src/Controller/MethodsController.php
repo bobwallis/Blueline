7882ad5ddd97099a5a558da4e2eb5c9cc49681f1
Bob Wallis <bob.wallis@gmail.com>
2022-10-16 17:10:48 +0100

    Missing use

--- src/Controller/MethodsController.php
+++ src/Controller/MethodsController.php
@@ -5,6 +5,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Doctrine\ORM\EntityManagerInterface;
