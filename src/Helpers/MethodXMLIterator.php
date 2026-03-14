167eecb97b8319fd4ae4525216fba38ab831327b
Bob Wallis <bob.wallis@gmail.com>
2026-03-14 22:15:47 +0000

    Add return type declarations to Iterator methods in MethodXMLIterator

--- src/Helpers/MethodXMLIterator.php
+++ src/Helpers/MethodXMLIterator.php
@@ -237,27 +237,27 @@ class MethodXMLIterator implements \Iterator, \Countable
     }
 
     // Implement the Iterator interface by borrowing it from the underlying array
-    public function rewind()
+    public function rewind(): void
     {
-        return reset($this->data);
+        reset($this->data);
     }
-    public function current()
+    public function current(): mixed
     {
         return current($this->data);
     }
-    public function key()
+    public function key(): mixed
     {
         return key($this->data);
     }
-    public function next()
+    public function next(): void
     {
-        return next($this->data);
+        next($this->data);
     }
-    public function valid()
+    public function valid(): bool
     {
         return key($this->data) !== null;
     }
-    public function count()
+    public function count(): int
     {
         return count($this->data);
     }
