821f245ac6fa3e152bedeb3dce95f14cd26fd82d
Bob Wallis <bob.wallis@gmail.com>
2026-03-29 20:54:56 +0000

    Remoe runtime mime-type registrations. Register PNG in YAML config as relevant to app routing

--- src/EventListener/RequestListener.php
+++ src/EventListener/RequestListener.php
@@ -15,18 +15,6 @@ class RequestListener
 
     public function onKernelRequest(RequestEvent $event)
     {
-        // Add some missing mime-types
-        $event->getRequest()->setFormat('jsonp', 'application/javascript');
-        $event->getRequest()->setFormat('manifest', 'text/cache-manifest');
-        $event->getRequest()->setFormat('ico', 'image/x-icon');
-        $event->getRequest()->setFormat('gif', 'image/gif');
-        $event->getRequest()->setFormat('png', 'image/png');
-        $event->getRequest()->setFormat('jpg', 'image/jpg');
-        $event->getRequest()->setFormat('svg', 'image/svg+xml');
-        $event->getRequest()->setFormat('pdf', 'application/pdf');
-        $event->getRequest()->setFormat('woff', 'application/font-woff');
-        $event->getRequest()->setFormat('woff2', 'application/font-woff2');
-
         // Set global parameters that can be used in @Cache annotations and other places
         $event->getRequest()->attributes->set('endpoint', $this->params->get('blueline.endpoint'));
         if ($this->kernelEnvironment == 'prod') {
